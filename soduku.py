3159f98c899bbc0605dd4da82034ddbeb177b0ad
Ryan <ryanvosbigian@gmail.com>
2013-09-06 19:58:17 -0700

    Update soduku.py

--- soduku.py
+++ soduku.py
@@ -6,6 +6,9 @@ print "Welcome to Soduku. Watch, interact, and admire."
 
 AI= raw_input("What level of intelligence do you want? A fly's brain (1), cyberman (2), or dalek (3). 1, 2, or 3?: ")
 
+Commit summary: Extended description: (optional)
+ryanvosbigian ryanvosbigian@gmail.com
+
 
 
 def flyexplode():
@@ -213,7 +216,107 @@ class Position():
 
 	
 def daleksolve(board):
-	Position.add(board)
+	P00= Position(board,[0,0])
+	P01= Position(board,[0,1])
+	P02= Position(board,[0,2])
+	P03= Position(board,[0,3])
+	P04= Position(board,[0,4])
+	P05= Position(board,[0,5])
+	P06= Position(board,[0,6])
+	P07= Position(board,[0,7])
+	P08= Position(board,[0,8])
+	P10= Position(board,[1,0])
+	P11= Position(board,[1,1])
+	P12= Position(board,[1,2])
+	P13= Position(board,[1,3])
+	P14= Position(board,[1,4])
+	P15= Position(board,[1,5])
+	P16= Position(board,[1,6])
+	P17= Position(board,[1,7])
+	P18= Position(board,[1,8])
+	P20= Position(board,[2,0])
+	P21= Position(board,[2,1])
+	P22= Position(board,[2,2])
+	P23= Position(board,[2,3])
+	P24= Position(board,[2,4])
+	P25= Position(board,[2,5])
+	P06= Position(board,[2,6])
+	P27= Position(board,[2,7])
+	P28= Position(board,[2,8])
+	P30= Position(board,[3,0])
+	P31= Position(board,[3,1])
+	P32= Position(board,[3,2])
+	P33= Position(board,[3,3])
+	P34= Position(board,[3,4])
+	P35= Position(board,[3,5])
+	P36= Position(board,[3,6])
+	P37= Position(board,[3,7])
+	P38= Position(board,[3,8])
+	P40= Position(board,[4,0])
+	P41= Position(board,[4,1])
+	P42= Position(board,[4,2])
+	P43= Position(board,[4,3])
+	P44= Position(board,[4,4])
+	P45= Position(board,[4,5])
+	P46= Position(board,[4,6])
+	P47= Position(board,[4,7])
+	P48= Position(board,[4,8])
+	P50= Position(board,[5,0])
+	P51= Position(board,[5,1])
+	P52= Position(board,[5,2])
+	P53= Position(board,[5,3])
+	P54= Position(board,[5,4])
+	P55= Position(board,[5,5])
+	P56= Position(board,[5,6])
+	P57= Position(board,[5,7])
+	P58= Position(board,[5,8])
+	P60= Position(board,[6,0])
+	P61= Position(board,[6,1])
+	P62= Position(board,[6,2])
+	P63= Position(board,[6,3])
+	P64= Position(board,[6,4])
+	P65= Position(board,[6,5])
+	P66= Position(board,[6,6])
+	P67= Position(board,[6,7])
+	P68= Position(board,[6,8])
+	P70= Position(board,[7,0])
+	P71= Position(board,[7,1])
+	P72= Position(board,[7,2])
+	P73= Position(board,[7,3])
+	P74= Position(board,[7,4])
+	P75= Position(board,[7,5])
+	P76= Position(board,[7,6])
+	P77= Position(board,[7,7])
+	P78= Position(board,[7,8])
+	P80= Position(board,[8,0])
+	P81= Position(board,[8,1])
+	P82= Position(board,[8,2])
+	P83= Position(board,[8,3])
+	P84= Position(board,[8,4])
+	P85= Position(board,[8,5])
+	P86= Position(board,[8,6])
+	P87= Position(board,[8,7])
+	P88= Position(board,[8,8])
+	
+	if P00[1] or P01[1] or P02[1] or P03[1] or P04[1] or P05[1] or P06[1] or P07[1] or P08[1] == True:
+		daleksolve(board)
+	if P10[1] or P11[1] or P12[1] or P13[1] or P14[1] or P15[1] or P16[1] or P17[1] or P18[1] == True:
+		daleksolve(board)
+	if P20[1] or P21[1] or P22[1] or P23[1] or P24[1] or P25[1] or P26[1] or P27[1] or P28[1] == True:
+		daleksolve(board)
+	if P30[1] or P31[1] or P32[1] or P33[1] or P34[1] or P35[1] or P36[1] or P37[1] or P38[1] == True:
+		daleksolve(board)
+	if P40[1] or P41[1] or P42[1] or P43[1] or P44[1] or P45[1] or P46[1] or P47[1] or P48[1] == True:
+		daleksolve(board)
+	if P50[1] or P51[1] or P52[1] or P53[1] or P54[1] or P55[1] or P56[1] or P57[1] or P58[1] == True:
+		daleksolve(board)
+	if P60[1] or P61[1] or P62[1] or P63[1] or P64[1] or P65[1] or P66[1] or P67[1] or P68[1] == True:
+		daleksolve(board)
+	if P70[1] or P71[1] or P72[1] or P73[1] or P74[1] or P75[1] or P76[1] or P77[1] or P78[1] == True:
+		daleksolve(board)
+	if P80[1] or P81[1] or P82[1] or P83[1] or P84[1] or P85[1] or P86[1] or P87[1] or P88[1] == True:
+		daleksolve(board)
+	
 	def composs(board,pos): #compute possibilities. (board, position (string... "xy" example "10)")
 		print board
 		possnum = ["2","1"]
